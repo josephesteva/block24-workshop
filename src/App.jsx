fad460208ce7376feb5aeac8b8e481771a1f0816
josephesteva <joe.esteva@gmail.com>
2023-11-13 15:26:05 -0500

    Add puppy data and display puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import {puppyList} from './data.js'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-
+	const [puppies, setPuppies] = useState(puppyList);
+	// console.log(puppies);
   return (
     <>
-
+			{puppies.map((puppy) => {
+				return <p key={puppy.id}>{puppy.name}</p>
+				})
+			}
     </>
   )
 }
