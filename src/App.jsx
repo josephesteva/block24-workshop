c8b942b575edeeeb0a656b794495e0eebd99d2b2
josephesteva <joe.esteva@gmail.com>
2023-11-13 16:43:31 -0500

    Implement puppy rendering app and update styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,28 @@ import { useState } from 'react';
 
 function App() {
 	const [puppies, setPuppies] = useState(puppyList);
+	const [featPupId, setFeatPupId] = useState(null);
+	const featuredPup = puppies.find((pup) => pup.id === featPupId)
 	// console.log(puppies);
   return (
     <>
 			{puppies.map((puppy) => {
-				return <p key={puppy.id}>{puppy.name}</p>
-				})
+				return <p onClick={() => {
+					setFeatPupId(puppy.id)
+					console.log(featuredPup)
+				}} key={puppy.id}>{puppy.name}</p>
+			})
 			}
+			{/* <p>{featPupId}</p> */}
+			{featPupId && (
+				<div>
+					<h2>{featuredPup.name}</h2>
+					<ul>
+						<li>Age: {featuredPup.age}</li>
+						<li>Email: {featuredPup.email}</li>
+					</ul>
+				</div>
+			)}
     </>
   )
 }
